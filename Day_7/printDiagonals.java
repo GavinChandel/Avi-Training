9a4bfd06ab090c00cdae56f3ef34f60e426c729b
Gavin Chandel <158019483+GavinChandel@users.noreply.github.com>
2024-02-02 17:38:13 +0530

    Add files via upload

---
+++ Day_7/printDiagonals.java
@@ -0,0 +1,13 @@
+package javaBasics.day7;
+
+public class printDiagonals {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length-i; j++){
+                System.out.print(matrix[j][j+i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
