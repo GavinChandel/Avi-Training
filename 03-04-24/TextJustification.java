63a7762532b0d15d5fabb51a3ef3c2b9f770dc1b
Gavin Chandel <158019483+GavinChandel@users.noreply.github.com>
2024-04-04 10:20:23 +0530

    Add files via upload

---
+++ 03-04-24/TextJustification.java
@@ -0,0 +1,61 @@
+package javaBasics.date_03_04_24;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class TextJustification {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        ArrayList<String> res = new ArrayList<>();
+        int n=words.length, len=0, s=0, wl=0; boolean newline=true;
+        for(int i=0; i<n; ++i) {
+            len+=words[i].length();
+            if(newline) newline=false;
+            else len+=1;
+
+            if(len>maxWidth) {
+                res.add(justified(s,i,words,maxWidth,wl));
+                s=i;
+                len=words[i].length();
+                wl=0;
+            }
+            if(i==n-1) res.add(leftAligned(s,i+1,words,maxWidth,wl+words[i].length()));
+            wl+=words[i].length();
+        }
+        return res;
+    }
+    private String justified(int start, int end, String[] words, int maxWidth, int l) {
+        if(end-start==1) return leftAligned(start, end, words, maxWidth, l);
+        int extraSpace=maxWidth-l, eachSpace;
+        char[] str=new char[maxWidth];
+        for(int i=0;start<end; i+=eachSpace+words[start].length(), extraSpace-=eachSpace, ++start) {
+            eachSpace=(int)Math.ceil((double)extraSpace/(end-start-1));
+            if(start==end-1) eachSpace=0;
+            paste(words[start], i, eachSpace, str);
+        }
+        return new String(str);
+    }
+    private String leftAligned(int start, int end, String[] words, int maxW, int l) {
+        char[] str=new char[maxW];
+        for(int i=0;start<end;i+=words[start].length()+1, ++start)
+            paste(words[start], i, start==(end-1)?maxW-(i+words[start].length()):1, str);
+        return new String(str);
+    }
+    private void paste(String w, int i, int space, char[] str) {
+        for(int j=0;j<w.length(); str[i++]=w.charAt(j), ++j);
+        while(space-->0) str[i++]=' ';
+    }
+
+    public static void main(String[] args) {
+        TextJustification solution = new TextJustification();
+
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        int maxWidth = 16;
+
+        List<String> justifiedText = solution.fullJustify(words, maxWidth);
+
+        for (String line : justifiedText) {
+            System.out.println(line);
+        }
+    }
+}
+
---
+++ 03-04-24/WordLadder.java
@@ -0,0 +1,81 @@
+package javaBasics.date_03_04_24;
+
+import java.util.*;
+
+class WordLadder {
+    String b;
+    HashMap<String,Integer> map=new HashMap<>();
+    List<List<String>> ans=new ArrayList<>();
+
+    public void dfs(String end,List<String> seq){
+        if(end.equals(b)){
+            List<String> list=new ArrayList<>(seq);
+            Collections.reverse(list);
+            ans.add(list);
+            return ;
+        }
+        String word=end;
+        int steps=map.get(word);
+        for(int i=0;i<b.length();i++){
+            for(char j='a';j<='z';j++){
+                char[] helper=word.toCharArray();
+                helper[i]=j;
+                String str=new String(helper);
+                if(map.containsKey(str)&&map.get(str)+1==steps){
+                    seq.add(str);
+                    dfs(str,seq);
+                    seq.remove(seq.size()-1);
+                }
+            }
+        }
+    }
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        b=beginWord;
+        HashSet<String> set=new HashSet<>(wordList);
+        Queue<String> q=new LinkedList<>();
+        map.put(beginWord,1);
+        q.add(beginWord);
+        set.remove(b);
+        while(!q.isEmpty()){
+            String word=q.poll();
+            int step=map.get(word);
+            if(word.equals(endWord)){
+                break;
+            }
+            for(int i=0;i<b.length();i++){
+                for(char j='a';j<='z';j++){
+                    char[] helper=word.toCharArray();
+                    helper[i]=j;
+                    String str=new String(helper);
+                    if(set.contains(str)){
+                        q.add(str);
+                        map.put(str,step+1);
+                        set.remove(str);
+                    }
+                }
+            }
+        }
+        if(map.containsKey(endWord)){
+            List<String> solver=new ArrayList<>();
+            solver.add(endWord);
+            dfs(endWord,solver);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        WordLadder solution = new WordLadder();
+
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
+
+        List<List<String>> ladder = solution.findLadders(beginWord, endWord, wordList);
+        System.out.println("Ladders:");
+        for (List<String> l : ladder) {
+            System.out.println(l);
+        }
+    }
+}
+
